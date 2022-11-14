3e1d5e0cc9153faf38a82ddc4755384d284773b6
Dowan Kim <kdw021127@gmail.com>
2022-11-14 22:59:14 +0900

    각 기능별 Url 분리 (ex. ~/main/)

--- Friend/urls.py
+++ Friend/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', include('Main.urls')),
+    path('login/', include('Login.urls')),
+    path('bill/', include('BillPayment.urls')),
+    path('community/', include('Community.urls')),
+    path('notice/', include('Notice.urls')),
+    path('parking/', include('ParkingLot.urls')),
 ]
