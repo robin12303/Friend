0ddbbcda6aaeb0710c5b42dc0a7b9d601f070b34
Dowan Kim <kdw021127@gmail.com>
2022-11-18 21:51:38 +0900

    logout

--- Main/views.py
+++ Main/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+template = 'Main/index.html'
+context = {
+    'islogin' : True,
+    'id' : "adsf",
+    'pw' : "asdf",
+    'user_name': 'adsf',
+}
+
+def logout(request):
+    context
+    if request.method == 'POST':
+        context["islogin"] = False
+    return render(request, template, context)
+
 def index(request):
-    template = loader.get_template('Main/index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    context
+    return render(request,template, context)
