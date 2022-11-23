7eafb78f5b14a1e0d227f547f0ec568a1242281e
Dowan Kim <kdw021127@gmail.com>
2022-11-23 21:35:30 +0900

    connect to mainpage

--- BillPayment/views.py
+++ BillPayment/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+template = 'BillPayment/bill.html'
+context = {}
 
 def index(request):
-    return render(request, 'BillPayment/관리비고지서.html')
+    return render(request, template, context)
