aa88225397bbbc0efcf0d3a7f95daed6477479b5
Dowan Kim <kdw021127@gmail.com>
2022-12-10 22:09:43 +0900

    receipt 삭제

--- BillPayment/urls.py
+++ BillPayment/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('detail', views.detail, name='billpayment_detail'),
     path('pay', views.pay, name='billpayment_pay'),
     path('bill', views.bill, name='billpayment_bill'),
-    path('receipt', views.receipt, name='billpayment_receipt'),
 ]
